9f59b1bd0e4fbc1a1c09380d80c5ac8ce4d35685
Enrique Rivera <erivera7240@gmail.com>
2024-07-04 17:31:07 -0500

    chore: Refactor main.py for improved code organization and readability

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from utils.TreeClassNode import build_class_tree_dict
+from utils.JFileFinderAndParse import JavaFileParser
+from utils.TreeJsonConvert import TreeJsonConvert
 
+
+def main(root):
+    parser = JavaFileParser(root)
+    parser.parse_files()
+    classes = parser.get_classes()
+
+    class_tree_dict = build_class_tree_dict(classes)
+    tree_json = TreeJsonConvert(class_tree_dict)
+
+
+if __name__ == '__main__':
+    root = 'C:'
+    main(root)
