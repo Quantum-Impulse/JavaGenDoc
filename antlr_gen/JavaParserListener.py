c33ed688a03939990ac2b1107c8cb771fd9c7100
Enrique <erivera7240@gmail.com>
2024-07-06 22:41:22 -0500

    python lexer files and parser need to consider version of anltr install to create them

--- antlr_gen/JavaParserListener.py
+++ antlr_gen/JavaParserListener.py
@@ -1,6 +1,6 @@
-# Generated from JavaParser.g4 by ANTLR 4.8
+# Generated from JavaParser.g4 by ANTLR 4.13.0
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .JavaParser import JavaParser
 else:
     from JavaParser import JavaParser
---
+++ build.sh
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+antlr4 -v 4.13.0 -Dlanguage=Python3 Expr.g4
+python3 main.py
